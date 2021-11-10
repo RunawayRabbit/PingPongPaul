d4a9683a08acf6f51f97f43d6014971a37d5554f
ejt <RunawayRabbitGames@gmail.com>
2021-11-10 14:52:19 +0100

    Destructible Surfaces

---
+++ PingPongPaul/Assets/_Game/Scripts/DestructibleSurface.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleSurface : MonoBehaviour
+{
+	private static readonly List<DestructibleSurface> allDestructibleSurfaces = new List<DestructibleSurface>();
+
+	private void OnEnable() { allDestructibleSurfaces.Add( this ); }
+
+	private void OnDisable() { allDestructibleSurfaces.Remove( this ); }
+
+
+	public void ResetAllDestructibleSurfaces()
+	{
+		foreach( var surface in allDestructibleSurfaces ) { surface.gameObject.SetActive( true ); }
+	}
+
+	private void OnCollisionEnter2D( Collision2D other )
+	{
+		if( other.gameObject.layer == LayerMask.NameToLayer( "Ball" ) ) { gameObject.SetActive( false ); }
+	}
+}
