06412972fd334bf43c6ab29f2d8ee00e7a9b3879
ejt <RunawayRabbitGames@gmail.com>
2021-11-11 11:52:15 +0100

    Paul Resetting

--- PingPongPaul/Assets/_Game/Scripts/Ball.cs
+++ PingPongPaul/Assets/_Game/Scripts/Ball.cs
@@ -1,4 +1,3 @@
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Events;
 using Random = Freya.Random;
--- PingPongPaul/Assets/_Game/Scripts/Paul/Paul.cs
+++ PingPongPaul/Assets/_Game/Scripts/Paul/Paul.cs
@@ -1,15 +1,19 @@
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 
 public class Paul : MonoBehaviour
 {
 	private static List<Paul> allPauls = new List<Paul>();
-	private static string paulPrefabPath = "Assets/_Game/Prefab/Paul.prefab";
+	[SerializeField] private string prefabPath = "Assets/_Game/Prefab/Paul.prefab";
 
 	private Vector3 startingPosition;
 
 	private List<PaulBalance> balances;
+
+	private bool isAttachedToBall;
 
 	private void Awake()
 	{
@@ -29,31 +33,35 @@ public class Paul : MonoBehaviour
 
 	public static void ResetAllPauls()
 	{
-		var paulPrefab = PrefabUtility.LoadPrefabContents( paulPrefabPath );
 		foreach( var paul in allPauls.ToArray() )
 		{
-			Instantiate( paulPrefab, paul.startingPosition, Quaternion.identity );
+			//Instantiate( PrefabUtility.LoadPrefabContents( paul.prefabPath ), paul.startingPosition, Quaternion.identity );
+			Instantiate( paul.gameObject, paul.startingPosition, quaternion.identity );
 			Destroy( paul.gameObject );
 		}
 	}
 
 	public void BallHitPaul( GameObject ball )
 	{
-		print( "Ow!" );
-
+		isAttachedToBall = true;
 		foreach( var balancer in balances )
 		{
 			balancer.enabled = false;
 		}
 	}
 
+	private IEnumerator StandBackUp()
+	{
+		yield return new WaitForSeconds( 1.0f );
+		if(!isAttachedToBall)
+		{
+			foreach( var balancer in balances ) { balancer.enabled = true; }
+		}
+	}
+
 	public void BallLeavesPaul( GameObject ball )
 	{
-		print( "Thank god I'm not connected to a ball anymore" );
-
-		foreach( var balancer in balances )
-		{
-			balancer.enabled = true;
-		}
+		isAttachedToBall = true;
+		StartCoroutine( StandBackUp() );
 	}
 }
