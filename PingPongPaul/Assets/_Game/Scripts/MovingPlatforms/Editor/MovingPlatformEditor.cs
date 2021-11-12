c8c046da86f32ee6f768ab34db1b1bbf4cd00ff9
ejt <RunawayRabbitGames@gmail.com>
2021-11-12 14:44:48 +0100

    Update MovingPlatformEditor.cs
    Fixed the wait timer being wrong

--- PingPongPaul/Assets/_Game/Scripts/MovingPlatforms/Editor/MovingPlatformEditor.cs
+++ PingPongPaul/Assets/_Game/Scripts/MovingPlatforms/Editor/MovingPlatformEditor.cs
@@ -6,27 +6,27 @@ using UnityEngine;
 public class MovingPlatformEditor : Editor
 {
 	private BoxCollider2D _collider;
-	private MovingPlatform _platform;
+	private MovingPlatform platform;
 
 	private void OnEnable()
 	{
 		if( !this.target ) return;
-		_platform = (MovingPlatform) this.target;
-		_collider = _platform.GetComponent<BoxCollider2D>();
+		platform = (MovingPlatform) this.target;
+		_collider = platform.GetComponent<BoxCollider2D>();
 
-		_platform.MakeWaypointsArraySafe();
-		_platform.waypoints[0].position = _platform.transform.position;
+		platform.MakeWaypointsArraySafe();
+		platform.waypoints[0].position = platform.transform.position;
 	}
 
 	private void OnSceneGUI()
 	{
-		_platform.MakeWaypointsArraySafe();
+		platform.MakeWaypointsArraySafe();
 
-		if( !Application.isPlaying ) _platform.waypoints[0].position = _platform.transform.position;
+		if( !Application.isPlaying ) platform.waypoints[0].position = platform.transform.position;
 
-		int numIterations = _platform.waypoints.Length - 1;
+		int numIterations = platform.waypoints.Length - 1;
 
-		if( _platform.movementStyle == MovingPlatform.LoopType.LoopBackToTheStart ) numIterations++;
+		if( platform.movementStyle == MovingPlatform.LoopType.LoopBackToTheStart ) numIterations++;
 
 		using( new Handles.DrawingScope() )
 		{
@@ -36,16 +36,16 @@ public class MovingPlatformEditor : Editor
 				DrawWaypointLines( i );
 			}
 
-			for( var i = 0; i < _platform.waypoints.Length; i++ ) DrawWaypoints( i );
+			for( var i = 0; i < platform.waypoints.Length; i++ ) DrawWaypoints( i );
 		}
 	}
 
 	private void DrawWaypointLines( int index )
 	{
-		Vector3 currentPosition = _platform.waypoints[index].position;
-		int     next            = (index + 1) % _platform.waypoints.Length;
+		Vector3 currentPosition = platform.waypoints[index].position;
+		int     next            = (index + 1) % platform.waypoints.Length;
 
-		Handles.DrawLine( currentPosition, _platform.waypoints[next].position );
+		Handles.DrawLine( currentPosition, platform.waypoints[next].position );
 	}
 
 	private void DrawWaypoints( int index )
@@ -59,19 +59,19 @@ public class MovingPlatformEditor : Editor
 		var startColor = new Color( 0.1f, 1.0f, 0.2f, 0.7f );
 		var endColor   = new Color( 0.1f, 0.2f, 1.0f, 0.7f );
 
-		Vector3 currentPosition = _platform.waypoints[index].position;
+		Vector3 currentPosition = platform.waypoints[index].position;
 
 		using( new Handles.DrawingScope( Matrix4x4.TRS( currentPosition,
 														Quaternion.identity,
-														_platform.transform.localScale ) ) )
+														platform.transform.localScale ) ) )
 		{
-			Handles.color = Color.Lerp( startColor, endColor, (float) index / _platform.waypoints.Length );
+			Handles.color = Color.Lerp( startColor, endColor, (float) index / platform.waypoints.Length );
 
 			Handles.DrawWireCube( Vector3.zero, Vector3.one );
 
 			string waypointText = index == 0 ? "Starting Position" : $"Waypoint {index}";
 
-			Handles.Label( Vector3.zero, $"{waypointText}\nWait Time: {_platform.waypoints[0].waitTime} sec", style );
+			Handles.Label( Vector3.zero, $"{waypointText}\nWait Time: {platform.waypoints[index].waitTime} sec", style );
 
 			// Early-out waypoint zero.
 			if( index == 0
@@ -85,10 +85,10 @@ public class MovingPlatformEditor : Editor
 
 			if( EditorGUI.EndChangeCheck() )
 			{
-				Undo.RecordObject( _platform, "Moved EnemyPath Point" );
-				EditorUtility.SetDirty( _platform );
+				Undo.RecordObject( platform, "Moved EnemyPath Point" );
+				EditorUtility.SetDirty( platform );
 
-				_platform.waypoints[index].position = newPoint; // + platform.transform.position;
+				platform.waypoints[index].position = newPoint; // + platform.transform.position;
 			}
 
 	}
